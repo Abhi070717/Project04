84c3aeabfdc6363256e9ea63b0e1487b26491700
abhi070717 <abhibhawsar066@gmail.com>
2026-02-16 19:22:19 +0530

    -

---
+++ ORSProject-04/src/main/java/in/co/rays/testmodel/TestRoleModel.java
@@ -0,0 +1,136 @@
+package in.co.rays.testmodel;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import in.co.rays.bean.RoleBean;
+import in.co.rays.exception.ApplicationException;
+import in.co.rays.exception.DuplicateRecordException;
+import in.co.rays.model.RoleModel;
+
+public class TestRoleModel {
+
+	public static RoleModel model = new RoleModel();
+
+	public static void main(String[] args) {
+
+//		testAdd();
+		testUpdate();
+//		testDelete();
+//		testfindByPk();
+//		testFindByName();
+//		testUpdate();
+	}
+
+	public static void testAdd() {
+
+		RoleBean bean = new RoleBean();
+		bean.setName("student");
+		bean.setDescription("student");
+		bean.setCreatedBy("root");
+		bean.setModifiedBy("root");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		RoleModel model = new RoleModel();
+
+		try {
+			try {
+				model.add(bean);
+			} catch (DuplicateRecordException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void testUpdate() {
+		try {
+			RoleBean bean = model.findByPk(1L);
+			bean.setName("Abhishish");
+			bean.setDescription("B.Tech");
+			model.update(bean);
+
+			RoleBean updatedbean = model.findByPk(1L);
+
+			if (!"Admin".equals(updatedbean.getName())) {
+				System.out.println("Test Update Success");
+			}
+		} catch (ApplicationException | DuplicateRecordException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testDelete() {
+		try {
+			RoleBean bean = new RoleBean();
+			long pk = 1L;
+			bean.setId(pk);
+			model.delete(bean);
+			RoleBean deletedbean = model.findByPk(pk);
+			if (deletedbean != null) {
+				System.out.println("Test Delete fail");
+			}
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testfindByPk() {
+		try {
+			RoleBean bean = model.findByPk(1L);
+			if (bean == null) {
+				System.out.println("Test Find By PK fail");
+			}
+			System.out.println(bean.getId());
+			System.out.println(bean.getName());
+			System.out.println(bean.getDescription());
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testFindByName() {
+		try {
+			RoleBean bean = model.findByName("admin");
+			if (bean == null) {
+				System.out.println("Test Find By PK fail");
+			}
+			System.out.println(bean.getId());
+			System.out.println(bean.getName());
+			System.out.println(bean.getDescription());
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void testSearch() {
+		try {
+			RoleBean bean = new RoleBean();
+			List list = new ArrayList();
+			bean.setName("student");
+			list = model.search(bean, 0, 0);
+			if (list.size() < 0) {
+				System.out.println("Test Serach fail");
+			}
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				bean = (RoleBean) it.next();
+				System.out.println(bean.getId());
+				System.out.println(bean.getName());
+				System.out.println(bean.getDescription());
+			}
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ORSProject-04/src/main/java/in/co/rays/testmodel/TestUserModel.java
@@ -0,0 +1,142 @@
+package in.co.rays.testmodel;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import in.co.rays.bean.UserBean;
+import in.co.rays.model.UserModel;
+
+public class TestUserModel {
+
+	public static void main(String[] args) throws Exception {
+
+		// addTest();
+		// updateTest();
+		// deleteTest();
+		// findByPkTest();
+		// findByLoginTest();
+		// authenticateTest();
+		searchTest();
+
+	}
+
+	public static void addTest() throws Exception {
+
+		UserBean bean = new UserBean();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		bean.setFirstName("Ram");
+		bean.setLastName("Sharma");
+		bean.setLogin("ram@gmail.com");
+		bean.setPassword("123");
+		bean.setDob(sdf.parse("2000-01-01"));
+		bean.setMobileNo("9999999999");
+		bean.setRoleId(2);
+		bean.setGender("Male");
+		bean.setCreatedBy("Admin");
+		bean.setModifiedBy("Admin");
+		bean.setCreatedDatetime(new Timestamp(System.currentTimeMillis()));
+		bean.setModifiedDatetime(new Timestamp(System.currentTimeMillis()));
+
+		UserModel model = new UserModel();
+
+		long pk = model.add(bean);
+
+		System.out.println("User added with PK = " + pk);
+
+	}
+
+	public static void updateTest() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = model.findByPk(1);
+
+		bean.setFirstName("UpdatedName");
+
+		model.update(bean);
+
+		System.out.println("User Updated");
+
+	}
+
+	public static void deleteTest() throws Exception {
+
+		UserBean bean = new UserBean();
+
+		bean.setId(1);
+
+		UserModel model = new UserModel();
+
+		model.delete(bean);
+
+		System.out.println("User Deleted");
+
+	}
+
+	public static void findByPkTest() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = model.findByPk(1);
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getFirstName());
+		System.out.println(bean.getLogin());
+
+	}
+
+	public static void findByLoginTest() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = model.findByLogin("ram@gmail.com");
+
+		System.out.println(bean.getId());
+		System.out.println(bean.getFirstName());
+		System.out.println(bean.getLogin());
+
+	}
+
+	public static void authenticateTest() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = model.authenticate("ram@gmail.com", "123");
+
+		if (bean != null) {
+
+			System.out.println("Login Success");
+			System.out.println(bean.getFirstName());
+
+		} else {
+
+			System.out.println("Login Failed");
+
+		}
+
+	}
+
+	public static void searchTest() throws Exception {
+
+		UserModel model = new UserModel();
+
+		UserBean bean = new UserBean();
+
+		bean.setFirstName("Ram");
+
+		List<UserBean> list = model.search(bean);
+
+		for (UserBean u : list) {
+
+			System.out.println(u.getId());
+			System.out.println(u.getFirstName());
+			System.out.println(u.getLogin());
+
+		}
+
+	}
+
+}
