78502824f5a0d1b2fae8abd3682df43a7a4556a0
abhi070717 <abhibhawsar066@gmail.com>
2026-02-18 19:49:50 +0530

    -

--- ORSProject-04/src/main/java/in/co/rays/model/RoleModel.java
+++ ORSProject-04/src/main/java/in/co/rays/model/RoleModel.java
@@ -3,8 +3,11 @@ package in.co.rays.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import in.co.rays.bean.RoleBean;
+import in.co.rays.exception.ApplicationException;
 import in.co.rays.util.JDBCDataSource;
 
 public class RoleModel {
@@ -34,7 +37,6 @@ public class RoleModel {
 		pk = nextPk();
 		conn = JDBCDataSource.getConnection();
 		conn.setAutoCommit(false);
-		System.out.println("Java is connected with MYSQL Successfully");
 		PreparedStatement pstmt = conn.prepareStatement("insert into st_role values(?, ?, ?, ?, ?, ?, ?)");
 		pstmt.setInt(1, pk);
 		pstmt.setString(2, bean.getName());
@@ -50,7 +52,6 @@ public class RoleModel {
 		conn.commit();
 		pstmt.close();
 		JDBCDataSource.closeConnection(conn);
-
 		return pk;
 
 	}
@@ -65,13 +66,13 @@ public class RoleModel {
 		PreparedStatement pstmt = conn.prepareStatement(
 				"update st_role set name = ?, description = ?, created_by = ?, modified_by = ?, created_datetime = ?, modified_datetime = ? where id = ?");
 
-		pstmt.setLong(7, bean.getId());
 		pstmt.setString(1, bean.getName());
 		pstmt.setString(2, bean.getDescription());
 		pstmt.setString(3, bean.getCreatedBy());
 		pstmt.setString(4, bean.getModifiedBy());
 		pstmt.setTimestamp(5, bean.getCreatedDatetime());
 		pstmt.setTimestamp(6, bean.getModifiedDatetime());
+		pstmt.setLong(7, bean.getId());
 		int i = pstmt.executeUpdate();
 
 		System.out.println(i + " Query OK, The rows affected (0.02 sec)" + "\n"
@@ -82,7 +83,7 @@ public class RoleModel {
 		conn.close();
 	}
 
-	public void Delete(RoleBean bean) throws Exception {
+	public void delete(RoleBean bean) throws Exception {
 
 		Connection conn = null;
 
@@ -101,6 +102,78 @@ public class RoleModel {
 		conn.commit();
 		pstmt.close();
 		conn.close();
+
+	}
+
+	public RoleBean findByPk(long Pk) throws ApplicationException {
+
+		Connection conn = null;
+		RoleBean bean = null;
+
+		StringBuffer sb = new StringBuffer("select * from st_role where id = ?");
+
+		conn = JDBCDataSource.getConnection();
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setLong(1, Pk);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				bean = new RoleBean();
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setDescription(rs.getString(3));
+				bean.setCreatedBy(rs.getString(4));
+				bean.setModifiedBy(rs.getString(5));
+				bean.setCreatedDatetime(rs.getTimestamp(6));
+				bean.setModifiedDatetime(rs.getTimestamp(7));
+
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ApplicationException("Exception : Exception in getting User by pk");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		return bean;
+
+	}
+
+	public static List<RoleBean> search(RoleBean bean) throws ApplicationException {
+		StringBuffer sb = new StringBuffer("select * from st_role where 1=1");
+
+		Connection conn = null;
+		ArrayList<RoleBean> list = new ArrayList<RoleBean>();
+
+		conn = JDBCDataSource.getConnection();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sb.toString());
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				bean = new RoleBean();
+				bean.setId(rs.getLong(1));
+				bean.setName(rs.getString(2));
+				bean.setDescription(rs.getString(3));
+				bean.setCreatedBy(rs.getString(4));
+				bean.setModifiedBy(rs.getString(5));
+				bean.setCreatedDatetime(rs.getTimestamp(6));
+				bean.setModifiedDatetime(rs.getTimestamp(7));
+				list.add(bean);
+			}
+			rs.close();
+			pstmt.close();
+
+		} catch (Exception e) {
+			throw new ApplicationException("Exception : Exception in search Role");
+		} finally {
+			JDBCDataSource.closeConnection(conn);
+		}
+		System.out.println(list.size());
+		return list;
 
 	}
 
--- ORSProject-04/src/main/java/in/co/rays/testmodel/TestRoleModel.java
+++ ORSProject-04/src/main/java/in/co/rays/testmodel/TestRoleModel.java
@@ -1,13 +1,17 @@
 package in.co.rays.testmodel;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import in.co.rays.bean.RoleBean;
+import in.co.rays.exception.ApplicationException;
 import in.co.rays.model.RoleModel;
 
 public class TestRoleModel {
-	
+
 	public static RoleModel model = new RoleModel();
 
 	public static void main(String[] args) throws Exception {
@@ -15,11 +19,12 @@ public class TestRoleModel {
 //		testnextPk();
 //		testAdd();
 //		testupdate();
-		testdelete();
+//		testdelete();
+//		testfindByPk();
+		testsearch();
 	}
 
 	public static void testnextPk() throws Exception {
-
 
 		model.nextPk();
 		System.out.println("NextPk method  Run");
@@ -40,11 +45,11 @@ public class TestRoleModel {
 		model.add(bean);
 		System.out.println("Data Stored in st_role");
 	}
-	
+
 	public static void testupdate() throws Exception {
-		
+
 		RoleBean bean = new RoleBean();
-		
+
 		bean.setId(1);
 		bean.setName("Abhishish Bhawsar");
 		bean.setDescription("B.Tech CSE(DS)");
@@ -52,20 +57,58 @@ public class TestRoleModel {
 		bean.setModifiedBy("User");
 		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
 		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
-		
+
 		model.update(bean);
 		System.out.println("Data Updated in st_role");
 	}
-	
+
 	public static void testdelete() throws Exception {
-		
+
 		RoleBean bean = new RoleBean();
-		
+
 		bean.setId(1);
-		
-		model.Delete(bean);
+
+		model.delete(bean);
 		System.out.println("Data Deleted in st_role");
-		
+
 	}
 
+	public static void testfindByPk() {
+
+		try {
+			RoleBean bean = model.findByPk(1L);
+			if (bean == null) {
+				System.out.println("Test FindByPk Failed");
+			}
+			System.out.println(bean.getId());
+			System.out.println(bean.getName());
+			System.out.println(bean.getDescription());
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void testsearch() {
+
+		try {
+			RoleBean bean = new RoleBean();
+			List list = new ArrayList();
+			bean.setName("Rajvansh");
+			list = model.search(bean);
+			if (list.size() < 0) {
+				System.out.println("Test Serach fail");
+			}
+			Iterator it = list.iterator();
+			while (it.hasNext()) {
+				bean = (RoleBean) it.next();
+
+				System.out.println(bean.getId());
+				System.out.println(bean.getName());
+				System.out.println(bean.getDescription());
+			}
+		} catch (ApplicationException e) {
+			e.printStackTrace();
+		}
+	}
 }
